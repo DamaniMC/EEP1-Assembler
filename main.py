384e5850a0e8237ef51dd774f4433c323594a6e4
DamaniMC <84398982+DamaniMC@users.noreply.github.com>
2022-02-12 02:05:11 +0000

    Fixed so that it now works with negative decimal numbers.

--- main.py
+++ main.py
@@ -9,80 +9,95 @@ OPS = {
     'LSL': 0x7,
     'LDR': 0x8,
     'LSR': 0x9,
-
 }
-OPCJUMP={
-    'JMP':0xC0 ,
-    'NOT':0xC1 ,
-    'JNE':0xC2 ,
-    'JEQ':0xC3 ,
-    'JCS':0xC4 ,
-    'JCC':0xC5 ,
-    'JMI':0xC6 ,
-    'JPL':0xC7 ,
-    'JGE':0xC8 ,
-    'JLT':0xC9 ,
-    'JGT':0xCA ,
-    'JLE':0xCB ,
-    'JHI':0xCC ,
-    'JLS':0xCD ,
-    'JSR':0xCE ,
-    'RET':0xCF ,
+OPCJUMP = {
+    'JMP': 0xC0,
+    'NOT': 0xC1,
+    'JNE': 0xC2,
+    'JEQ': 0xC3,
+    'JCS': 0xC4,
+    'JCC': 0xC5,
+    'JMI': 0xC6,
+    'JPL': 0xC7,
+    'JGE': 0xC8,
+    'JLT': 0xC9,
+    'JGT': 0xCA,
+    'JLE': 0xCB,
+    'JHI': 0xCC,
+    'JLS': 0xCD,
+    'JSR': 0xCE,
+    'RET': 0xCF,
 }
 
-ar=["MOV","R0","#0x10"]
 
 
+#So python uses signed and magnitude binary not two's complement
+#I fix this using this two_comp function below that basically puts the number is twoscomplement for, the integer value won't be signed anymore
+def two_comp(n,nBits):
+  if n<0:
+    mask = (1 << nBits)-1
+    return (~n ^ mask);
+  else:
+    return n
+#Changes the Register format from RN to just N
 def convReg(r):
-  return int(r.replace("R",""))
+    return int(r.replace("R", ""))
 
-def convImm(Imm):
+
+
+#Changes Immediate format by removing the # and converting to decimal
+def convImm(Imm,nBits):
     if "#" == Imm[0]:
-      Imm=Imm[1:]
+        Imm = Imm[1:]
     if "0x" == Imm[0:2]:
-      return int(Imm,16)
+        return int(Imm, 16)
     elif "0b" == Imm[0:2]:
-      return int(Imm,2)
+        return int(Imm, 2)
     else:
-      return int(Imm)
+        return two_comp(int(Imm),nBits)
 
+
+#translates a single line
 def translate(a):
-    if ( a[0] not in OPS.keys() ) and ( a[0] not in OPCJUMP.keys()):
-        print("Invalid OPC :",a[0])
+    #Check wether the opcode is valid
+    if (a[0] not in OPS.keys()) and (a[0] not in OPCJUMP.keys()):
+        print("Invalid OPC :", a[0])
         return -1
     #Creates the Operand format
-    if len(a)==4:
-        i8=0<<8
-        i5_7=convReg(a[2])<<5#takes the register number then shifts accordingly
-        i0_4=convImm(a[3]) # converts 5bits operand to an integer
-        operand=i5_7+i0_4 #adds together
+    if len(a) == 4:
+        i8 = 0 << 8
+        i5_7 = convReg(
+            a[2]) << 5  #takes the register number then shifts accordingly
+        i0_4 = convImm(a[3],5)  # converts 5bits operand to an integer
+        operand = i5_7 + i0_4  #adds together
     else:
-        i8=1<<8
-        i0_7=convImm(a[2])
-        operand=i0_7
+        i8 = 1 << 8
+        i0_7 = convImm(a[2],8)
+        operand = i0_7
     #Creates the Opcode format
-    if "J"in a[0]:  
-      opcode=int(OPCJUMP[a[0]])<<8
+    if "J" in a[0]:
+        opcode = int(OPCJUMP[a[0]]) << 8
     else:
-      i12_15=int(OPS[a[0]])<<12
-      i9_11 =convReg(a[1])<<9
-      opcode=i8+i9_11+i12_15
+        i12_15 = int(OPS[a[0]]) << 12
+        i9_11 = convReg(a[1]) << 9
+        opcode = i8 + i9_11 + i12_15
     #creates assemly
-    return hex(operand+opcode)
-#translate(ar)
+    return hex(operand + opcode)
 
-infile = open("assembly.txt","r")
-outfile = open("machine.ram","w")
 
+infile = open("assembly.txt", "r")
+outfile = open("machine.ram", "w")
 address = 0
 for line in infile:
-  line.replace(",","")
-  split = line.split()
-  machine=translate(split)
-  toWrite=str(str(hex(address))+" "+str(machine)+"\n")
-  print(toWrite)
-  outfile.write(toWrite)
-  address+=1
+    line = line.replace(",", "")
+    split = line.split()
+    machine = translate(split)
+    toWrite = str(str(hex(address)) + " " + str(machine) + "\n")
+    print(toWrite)
+    outfile.write(toWrite)
+    address += 1
 infile.close()
 outfile.close()
 #print(translate(ar))
+
+print("this onyl")
